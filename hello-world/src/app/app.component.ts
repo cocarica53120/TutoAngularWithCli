27d4b12507ca776c515631471c842c7f15e54bce
cocarica53120 <alain.collet2@gmail.com>
2020-11-10 00:20:10 +0100

    Fix lint warnings....

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -8,34 +8,34 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hello-world';
   imageUrl = 'http://lorempixel.com/400/200';
-  isActive = false;// class binding
-  tel = "01234";// 2 way binding
-  course = {// For pipes
-    title: "The Angular course",
+  isActive = false; // class binding
+  tel = '01234'; // 2 way binding
+  course = { // For pipes
+    title: 'The Angular course',
     rating: 4.95785,
     students: 258980,
     price: 10.50,
     releaseDate: new Date(2020, 1, 12)
-  }
+  };
   text = 'A very long text , which is really toooooooooooooooooooooooooooooo lonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnggggggggg';
-  onDiv() {
+  onDiv(): void {
     console.log('Div was clicked');
   }
 
-  onSave(event: any) {
-    event.stopPropagation();// To stop bubble propagation on outer div
+  onSave(event: any): void {
+    event.stopPropagation(); // To stop bubble propagation on outer div
     console.log('Save was clicked', event.target);
   }
 
-  onKeyUp(event: any) {
-    console.log("ENTER was pressed", "target is", event.target);
+  onKeyUp(event: any): void {
+    console.log('ENTER was pressed', 'target is', event.target);
   }
 
-  onKeyUpValue(value: string) {
-    console.log("ENTER was pressed", "value is", value);
+  onKeyUpValue(value: string): void {
+    console.log('ENTER was pressed', 'value is', value);
   }
 
-  onKeyUpTel() {
-    console.log("Tel was pressed", this.tel);
+  onKeyUpTel(): void {
+    console.log('Tel was pressed', this.tel);
   }
 }
--- hello-world/src/app/summary.pipe.ts
+++ hello-world/src/app/summary.pipe.ts
@@ -4,11 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'summary',
 })
 export class SummaryPipe implements PipeTransform {
-  transform(value: string, limit?: number) {
+
+  transform(value: string, limit?: number): string {
     if (!value) {
       return null;
     }
-    let actLimit = (limit) ? limit : 50;
+    const actLimit = (limit) ? limit : 50;
     return value.substr(0, actLimit) + '.....';
    }
 }
